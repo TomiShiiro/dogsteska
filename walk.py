ccaa6e781f5daae95483a0cf80ddfbfa454f959e
i3umbac <wendan@students.zcu.cz>
2025-03-12 11:30:39 +0100

    Create walk.py

---
+++ walk.py
@@ -0,0 +1,15 @@
+# Contains methods used to make the dog walk
+
+# Authors: Andy Wendler, Ondřej Všelko
+# Created: 11. 3. 2025
+# Last edited: 12. 3. 2025
+
+
+# test at techtower
+# https://github.com/unitreerobotics/unitree_sdk2_python
+
+# the dog will walk straight for a distance
+# params:
+#   dist - distance to walk
+def straightWalk(dist):
+    print("I'm walking")
